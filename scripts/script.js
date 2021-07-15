25f860e0e53fdc0c0a229e337688406ff31c68c2
rrm1 <rustamramazanovworkmail@gmail.com>
2021-07-15 15:00:56 +0300

    (add)get goods

--- scripts/script.js
+++ scripts/script.js
@@ -78,3 +78,54 @@ cartOverlay.addEventListener('click', event => {
     cartModalClose();
   }
 });
+
+// получение товаров
+try {
+  const goodsList = document.querySelector('.goods__list');
+
+  if (!goodsList) {
+    throw 'This is not a goods page!';
+  }
+
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+
+    const li = document.createElement('li');
+
+    li.classList.add('goods__item');
+
+    li.innerHTML = `
+      <article class="good">
+        <a class="good__link-img" href="card-good.html#${id}">
+            <img class="good__img" src="goods-image/${preview}" alt="">
+        </a>
+        <div class="good__description">
+            <p class="good__price">${cost} &#8381;</p>
+            <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+            ${sizes ?
+              `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+              ''
+            }
+    
+            <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+        </div>
+      </article>
+    `;
+
+    return li;
+  };
+
+  const renderGoodsList = data => {
+    goodsList.textContent = '';
+
+    data.forEach((item) => {
+      const card = createCard(item);
+      goodsList.append(card);
+    });
+  };
+
+  getGoods(renderGoodsList);
+
+
+} catch (err) {
+  console.warn(err);
+}

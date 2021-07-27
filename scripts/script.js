7399a3da478a05633963ffb4bf80b69a0111505f
rrm1 <rustamramazanovworkmail@gmail.com>
2021-07-28 00:17:46 +0300

    (add)choice of color and size of coods

--- scripts/script.js
+++ scripts/script.js
@@ -167,10 +167,14 @@ try {
   const cardGoodTitle = document.querySelector('.card-good__title');
   const cardGoodPrice = document.querySelector('.card-good__price');
   const cardGoodColor = document.querySelector('.card-good__color');
+  const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper');
   const cardGoodColorList = document.querySelector('.card-good__color-list');
   const cardGoodSizes = document.querySelector('.card-good__sizes');
   const cardGoodSizesList = document.querySelector('.card-good__sizes-list');
   const cardGoodBuy = document.querySelector('.card-good__buy');
+
+  const generateList = data => data.reduce((html, item, i) => html +
+    `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '');
 
   const renderCardGood = ([{ brand, name, cost, color, sizes, photo }]) => {
     cardGoodImage.src = `goods-image/${photo}`;
@@ -181,14 +185,35 @@ try {
 
     if (color) {
       cardGoodColor.textContent = color[0];
+      cardGoodColor.dataset.id = 0;
+      cardGoodColorList.innerHTML = generateList(color);
     } else {
       cardGoodColor.style.display = 'none';
     }
     if (sizes) {
       cardGoodSizes.textContent = sizes[0];
+      cardGoodSizes.dataset.id = 0;
+      cardGoodSizesList.innerHTML = generateList(sizes);
     } else {
       cardGoodSizes.style.diplay = 'none';
     }
+
+    cardGoodSelectWrapper.forEach(item => {
+      item.addEventListener('click', e => {
+        const target = e.target;
+
+        if (target.closest('.card-good__select')) {
+          target.classList.toggle('card-good__select__open');
+        }
+
+        if (target.closest('.card-good__select-item')) {
+          const cardGoodSelect = item.querySelector('.card-good__select');
+          cardGoodSelect.textContent = target.textContent;
+          cardGoodSelect.dataset.id = target.dataset.id;
+          cardGoodSelect.classList.remove('card-good__select__open');
+        }
+      });
+    });
   };
 
   getGoods(renderCardGood, 'id', hash);
